621ffd87ea811206101c8585b4093d73e9f98965
friranger <65720979+friranger1@users.noreply.github.com>
2025-11-30 13:28:37 +0200

    Fixed sortList function

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,40 +1,31 @@
 'use strict';
+
 const salaryElems = [...document.querySelectorAll('[data-salary]')];
 const list = document.getElementsByTagName('ul')[0];
-const sortedArray = sortList(salaryElems, 'salary');
-const people = getEmployees(salaryElems);
-console.log(people)
 
-sortedArray.forEach(li => {
-    list.appendChild(li);
-  });
+getEmployees(salaryElems);
+sortList(salaryElems, 'salary', list);
 
-
-
-
-function sortList(array, attribute) {
-  const sortedArray = array.sort((elem, elem2)=> {
-    const val = elem.dataset[attribute];
-    const cleaned = val.replace(/[^\d.,]/g, "");
-    const noCommas = cleaned.replace(',', '');
-    const num = parseFloat(noCommas);
-
-    const val2 = elem2.dataset[attribute];
-    const cleaned2 = val2.replace(/[^\d.,]/g, "");
-    const noCommas2 = cleaned2.replace(',', '');
-    const num2 = parseFloat(noCommas2);
+function sortList(array, attribute, listElement) {
+  const sortedArray = array.sort((elem, elem2) => {
+    const num = parseFloat(elem.dataset[attribute].replace(/[^\d.,]/g, '').replace(',', ''));
+    const num2 = parseFloat(elem2.dataset[attribute].replace(/[^\d.,]/g, '').replace(',', ''));
     return num2 - num;
   });
-  return sortedArray;
+
+  sortedArray.forEach((li) => {
+    listElement.appendChild(li);
+  });
 }
 
-function getEmployees(list) {
-  const array = list.map(element => {
+
+function getEmployees(l) {
+  const array = l.map((element) => {
     return {
-      name: element.textContent.trim(), // имя из текста <li>
+      name: element.textContent.trim(),
       position: element.dataset.position,
       age: parseInt(element.dataset.age),
-      salary: parseFloat(element.dataset.salary.replace(/[$,]/g, ''))
+      salary: parseFloat(element.dataset.salary.replace(/[$,]/g, '')),
     };
   });
 

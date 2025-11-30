7961d26ed5e30f004fb0619de096a7a0ff42aa96
friranger <65720979+friranger1@users.noreply.github.com>
2025-11-30 13:12:18 +0200

    Add 2 functions and run both of them

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,42 @@
 'use strict';
+const salaryElems = [...document.querySelectorAll('[data-salary]')];
+const list = document.getElementsByTagName('ul')[0];
+const sortedArray = sortList(salaryElems, 'salary');
+const people = getEmployees(salaryElems);
+console.log(people)
 
-// write code here
+sortedArray.forEach(li => {
+    list.appendChild(li);
+  });
+
+
+
+
+function sortList(array, attribute) {
+  const sortedArray = array.sort((elem, elem2)=> {
+    const val = elem.dataset[attribute];
+    const cleaned = val.replace(/[^\d.,]/g, "");
+    const noCommas = cleaned.replace(',', '');
+    const num = parseFloat(noCommas);
+
+    const val2 = elem2.dataset[attribute];
+    const cleaned2 = val2.replace(/[^\d.,]/g, "");
+    const noCommas2 = cleaned2.replace(',', '');
+    const num2 = parseFloat(noCommas2);
+    return num2 - num;
+  });
+  return sortedArray;
+}
+
+function getEmployees(list) {
+  const array = list.map(element => {
+    return {
+      name: element.textContent.trim(), // имя из текста <li>
+      position: element.dataset.position,
+      age: parseInt(element.dataset.age),
+      salary: parseFloat(element.dataset.salary.replace(/[$,]/g, ''))
+    };
+  });
+
+  return array;
+}
